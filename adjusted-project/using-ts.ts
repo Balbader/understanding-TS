0bd7a6173f9cc25fde9b5551c24bb75af1a735ec
Balbader <15971200+Balbader@users.noreply.github.com>
2022-02-15 20:21:58 +0100

    refactoring to queryselector

--- adjusted-project/using-ts.ts
+++ adjusted-project/using-ts.ts
@@ -1,6 +1,6 @@
-const button = document.querySelector("button");
-const input1 = document.getElementById("num1")! as HTMLInputElement;
-const input2 = document.getElementById("num2")! as HTMLInputElement;
+const button = document.querySelector('button');
+const input1 = document.querySelector('#num1')! as HTMLInputElement;
+const input2 = document.querySelector('#num2')! as HTMLInputElement;
 
 const add = (num1: number, num2: number) => {
   return num1 + num2;
